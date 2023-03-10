b6e955b5d4482ebe01d1c5837c96310a118bb86f
karlotimmerman <karlo.timmerman@skydust.io>
2023-03-10 13:24:33 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 from utils import parse_pdf, embed_text, get_answer
 
 st.header("Doc QA")
-uploaded_file = st.file_uploader("Upload a pdf", type=["pdf"])
+uploaded_file = st.file_uploader("Upload docs", type=["pdf"])
 
 if uploaded_file is not None:
     index = embed_text(parse_pdf(uploaded_file))
-    query = st.text_area("Ask a question about the document")
+    query = st.text_area("ask me all about the ESRS (EU Sustainability reporting")
     button = st.button("Submit")
     if button:
         st.write(get_answer(index, query))
